2da926fd07f3487d2e249f8a2a7007948608fc79
padev <pav-pas@mail.ru>
2026-09-01 10:31:03 +0500

    test

--- teacher_test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Тестовый запуск teacher.sh (как на проверке: изнутри mystery + MAIN_SUSPECT)
-cd "$(dirname "$0")/mystery" || exit 1
-
-export MAIN_SUSPECT="Harvey Dent"
-bash ../teacher.sh
