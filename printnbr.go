79473a11acfb428a8150947dc4b958ba6af4a872
padev <pav-pas@mail.ru>
2026-09-01 21:01:55 +0500

    printnbr.go v2

--- printnbr.go
+++ printnbr.go
@@ -5,21 +5,72 @@ import "github.com/01-edu/z01"
 func PrintNbr(n int) {
 	if n < 0 {
 		z01.PrintRune('-')
+
 		if n == -n {
-			n = -(n + 1)
-			printDigits(n)
-			z01.PrintRune('1')
+			printMinInt(n)
 			return
 		}
+
 		n = -n
 	}
 
 	printDigits(n)
 }
 
+func printMinInt(n int) {
+	if n <= -10 {
+		printMinInt(n / 10)
+	}
+
+	switch n % 10 {
+	case 0:
+		z01.PrintRune('0')
+	case -1:
+		z01.PrintRune('1')
+	case -2:
+		z01.PrintRune('2')
+	case -3:
+		z01.PrintRune('3')
+	case -4:
+		z01.PrintRune('4')
+	case -5:
+		z01.PrintRune('5')
+	case -6:
+		z01.PrintRune('6')
+	case -7:
+		z01.PrintRune('7')
+	case -8:
+		z01.PrintRune('8')
+	case -9:
+		z01.PrintRune('9')
+	}
+}
+
 func printDigits(n int) {
 	if n >= 10 {
 		printDigits(n / 10)
 	}
-	z01.PrintRune(rune(n%10 + '0'))
+
+	switch n % 10 {
+	case 0:
+		z01.PrintRune('0')
+	case 1:
+		z01.PrintRune('1')
+	case 2:
+		z01.PrintRune('2')
+	case 3:
+		z01.PrintRune('3')
+	case 4:
+		z01.PrintRune('4')
+	case 5:
+		z01.PrintRune('5')
+	case 6:
+		z01.PrintRune('6')
+	case 7:
+		z01.PrintRune('7')
+	case 8:
+		z01.PrintRune('8')
+	case 9:
+		z01.PrintRune('9')
+	}
 }
