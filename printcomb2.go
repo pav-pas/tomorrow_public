07eb91ca410acb25be18253624f597edf1489e6b
padev <pav-pas@mail.ru>
2026-09-01 20:35:16 +0500

    printcomb2 v2

--- printcomb2.go
+++ printcomb2.go
@@ -3,17 +3,23 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb2() {
-	for i := 0; i <= 98; i++ {
-		for j := i + 1; j <= 99; j++ {
-			z01.PrintRune(rune(i/10 + '0'))
-			z01.PrintRune(rune(i%10 + '0'))
-			z01.PrintRune(' ')
-			z01.PrintRune(rune(j/10 + '0'))
-			z01.PrintRune(rune(j%10 + '0'))
+	for a := '0'; a <= '9'; a++ {
+		for b := '0'; b <= '9'; b++ {
+			for c := a; c <= '9'; c++ {
+				for d := '0'; d <= '9'; d++ {
+					if a*10+b < c*10+d {
+						z01.PrintRune(a)
+						z01.PrintRune(b)
+						z01.PrintRune(' ')
+						z01.PrintRune(c)
+						z01.PrintRune(d)
 
-			if i != 98 || j != 99 {
-				z01.PrintRune(',')
-				z01.PrintRune(' ')
+						if a != '9' || b != '8' || c != '9' || d != '9' {
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
+						}
+					}
+				}
 			}
 		}
 	}
