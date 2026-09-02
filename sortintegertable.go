d1c99c4b025e61d8a696134d20f460d2caac2d62
Pavel Silenko <pavsilenko@gmail.com>
2026-09-02 12:45:13 +0500

    sortintegertable.go debug info

--- sortintegertable.go
+++ sortintegertable.go
@@ -1,13 +1,20 @@
 package piscine
 
+//import "fmt"
+
 func SortIntegerTable(table []int) {
 	n := len(table)
 	for i := 0; i < n; i++ {
 		for j := 0; j < n; j++ {
 			if table[i] < table[j] {
+
+				//fmt.Println(i, j)
+
 				a := table[i]
 				table[i] = table[j]
 				table[j] = a
+
+				//fmt.Println(table)
 			}
 		}
 	}
